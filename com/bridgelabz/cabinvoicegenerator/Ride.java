770888440ea016486caee7c26a31543b15274cc5
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-05-20 21:15:35 +0530

    Added: holds the details of rides

---
+++ com/bridgelabz/cabinvoicegenerator/Ride.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.cabinvoicegenerator;
+/*
+ *This class holds the details of rides
+ */
+public class Ride {
+    //variables
+    public float distance;
+    public int time;
+
+    //constructor
+    public Ride(float distance, int time){
+        this.distance = distance;
+        this.time = time;
+    }
+}
